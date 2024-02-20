521e5a95c9e280d84085adedff42a6b7982aecc4
Grubstar808 <espen86@gmail.com>
2024-02-20 16:09:58 +0100

    content

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const CyclicDB = require("@cyclic.sh/dynamodb");
+const db = CyclicDB(process.env.CYCLIC_DB);
+let contentdb = db.collection("content");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post("/", async function (req, res, next) {
+  const { content } = req.body;
+  await contentdb.set(Content, content);
+  res.end();
+});
+
+router.get("/", async function (req, res, next) {
+  let list = await contentdb.list();
+  res.send(list);
 });
 
 module.exports = router;
